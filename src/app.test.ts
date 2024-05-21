3042cb8b0e01064e94f7369e2ca5c939685ab1b2
Behzad Barzin <behzad_barzin@live.com>
2024-05-21 17:27:35 +0330

    Adds comment to test

--- src/app.test.ts
+++ src/app.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 
 import { createServer } from './app';
 
+// This is a test to make sure the server is running
 describe('Testing Server', () => {
   it("GET: / should return 'Hello World!'", async () => {
     const app = await createServer();
