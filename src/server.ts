496c30d284fea3ae45e32c0a05dba1f2ce6cebdd
Behzad Barzin <behzadbarzin1@gmail.com>
2024-05-23 23:03:42 +0330

    Changed default PORT=4321

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { createServer } from './app';
 
 // Because ./app is imported above, and it imports the env file, we can use the environment variables here
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4321;
 
 createServer().then((app) => {
   // Listen on provided port when app is ready to be launched
